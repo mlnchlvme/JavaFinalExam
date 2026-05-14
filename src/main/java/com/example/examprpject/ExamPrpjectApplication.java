5a5fc6a8df28ccb07e1806893cba0f79b4c372c2
Viktor Kim <viktorkim2702@gmail.com>
2026-05-14 13:00:05 +0500

    initial project structure

---
+++ src/main/java/com/example/examprpject/ExamPrpjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.examprpject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPrpjectApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(ExamPrpjectApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/example/examprpject/ExamPrpjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examprpject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPrpjectApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
